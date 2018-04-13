e6e8130212aac2fabc7dc12dbb5f09530edca25b
Elias Boiko <elias.boiko@gmail.com>
2018-04-13 23:02:26 +0300

    Add project files.

---
+++ aucassin 13 apr 18 night/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aucassin_13_apr_18_night
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
